71092c221f6bce6b8a0077f239fb5111dda46d77
Petar <petio567@abv.bg>
2021-11-04 22:05:03 +0200

    added an abstract logic for DbContext

--- YourMoviesForum/Data/YourMoviesForum.Data/YourMoviesDbContext.cs
+++ YourMoviesForum/Data/YourMoviesForum.Data/YourMoviesDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using YourMoviesForum.Data.Common.Models;
 using YourMoviesForum.Data.Configuration;
 using YourMoviesForum.Data.Models;
 
@@ -30,7 +33,54 @@ namespace YourMoviesForum
                 builder.UseSqlServer(DataSettings.ConnectionString);
             }
         }
+
+        public override int SaveChanges() => SaveChanges(true);
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            ApplyAuditforRules();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        //Disable cascade delete
+         protected override void OnModelCreating(ModelBuilder builder)
+        {
+            var entityTypes = builder.Model.GetEntityTypes().ToList();
+
+            var foreignKeys = entityTypes
+                .SelectMany(e => e.GetForeignKeys()
+                .Where(f => f.DeleteBehavior == DeleteBehavior.Cascade));
+
+            foreach (var foreignKey in foreignKeys)
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        //To apply abstractly DateTime everywhere with IAuditInfo when something is changed
+        private void ApplyAuditforRules()
+        {
+            var changedEntries = ChangeTracker
+                .Entries()
+                .Where(e =>
+                    e.Entity is IAuditInfo &&
+                    (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            foreach (var entry in changedEntries)
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State==EntityState.Added && entity.CreatedOn==default)
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
+        }
+
         //protected override void OnModelCreating(ModelBuilder builder)
-          //      => builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        //      => builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
     }
 }
