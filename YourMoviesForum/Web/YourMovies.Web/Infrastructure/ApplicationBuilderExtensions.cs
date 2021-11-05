e1f29611351764f44fbc9340402961eaac634477
Petar <petio567@abv.bg>
2021-11-05 19:21:17 +0200

    added a basic post seeder and ApplicationBuilderExtensions

---
+++ YourMoviesForum/Web/YourMovies.Web/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System.Linq;
+
+using YourMoviesForum;
+using YourMoviesForum.Data.Models;
+
+namespace YourMovies.Web.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        //checks whether there are new migrations and apply them when the program starts
+        public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
+        {
+           //Creating a scope where the code will exist
+           using var scopedServices = app.ApplicationServices.CreateScope();
+
+           var data=scopedServices.ServiceProvider.GetService<YourMoviesDbContext>();
+
+            data.Database.Migrate();
+
+            app.ApplicationServices.GetService<YourMoviesDbContext>();
+
+            return app;
+        }
+
+        public static void SeedPosts(YourMoviesDbContext data)
+        {
+            if (data.Posts.Any())
+            {
+                return;
+            }
+
+            data.Categories.AddRange(new[]
+                {
+                    new Category {Name="Sci-Fi"},
+                    new Category {Name="Action"},
+                    new Category {Name="Horror"},
+                    new Category {Name="Sci-Fi"},
+                    new Category {Name="Romantic"},
+                    new Category {Name="Cartoon"}
+                });
+        }
+    }
+}
--- YourMoviesForum/Web/YourMovies.Web/Startup.cs
+++ YourMoviesForum/Web/YourMovies.Web/Startup.cs
@@ -1,11 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using YourMoviesForum;
+using YourMovies.Web.Infrastructure;
 using YourMoviesForum.Services.Data;
 using YourMoviesForum.Services.Data.Posts;
 
@@ -47,6 +49,8 @@ namespace YourMovies.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.PrepareDatabase();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
