6b8974f5602dd8b9f0f05492f12b87a062ba0eaf
Petar <petio567@abv.bg>
2022-02-03 14:36:41 +0200

    added isAdmin method

--- YourMoviesForum/Web/YourMovies.Web/Infrastructure/ClaimsPrincipalExtensions.cs
+++ YourMoviesForum/Web/YourMovies.Web/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -1,10 +1,15 @@
 ﻿using System.Security.Claims;
 
+using static YourMoviesForum.Common.GlobalConstants.Administrator;
+
 namespace YourMovies.Web.Infrastructure
 {
     public static class ClaimsPrincipalExtensions
     {
         public static string Id(this ClaimsPrincipal user)
             => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        public static bool IsAdministrator(this ClaimsPrincipal claimsPrincipal)
+           => claimsPrincipal.IsInRole(AdministratorRoleName);
     }
 }
