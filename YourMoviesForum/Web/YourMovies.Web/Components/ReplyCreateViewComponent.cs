2257342a4a345c314230f8ccdb3d6a56dfd58ae3
Petar <petio567@abv.bg>
2022-02-27 18:24:56 +0200

    Created a createReplyView component for creating a reply and applied it to details post view

---
+++ YourMoviesForum/Web/YourMovies.Web/Components/ReplyCreateViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourMoviesForum.Web.InputModels.Replies;
+
+namespace YourMovies.Web.Components
+{
+    [ViewComponent(Name = "CreateReply")]
+    public class ReplyCreateViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(int? parentId, int postId)
+        {
+            var viewModel = new ReplyCreateInputModel
+            {
+                ParentId = parentId,
+                PostId = postId
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
