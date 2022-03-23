1eb2fe896390d1a17171f7c282a0c92244fb946d
Petar <petio567@abv.bg>
2022-03-24 00:18:27 +0200

    Added ChatController with View for Sending a message and a business logic for it

---
+++ YourMoviesForum/Services/YourMoviesForum.Services.Data/Messages/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace YourMoviesForum.Services.Data.Messages
+{
+    public interface IMessageService
+    {
+        Task CreateAsync(string content, string authorId, string receiverId);
+    }
+}
