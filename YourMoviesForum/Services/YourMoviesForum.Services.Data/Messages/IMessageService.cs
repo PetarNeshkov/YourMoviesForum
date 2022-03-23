db5758a067de5764a94c80c651b9a1cf59adbd18
Petar <petio567@abv.bg>
2022-03-24 00:19:19 +0200

    Revert "Added ChatController with View for Sending a message and a business logic for it"
    This reverts commit 1eb2fe896390d1a17171f7c282a0c92244fb946d.

--- YourMoviesForum/Services/YourMoviesForum.Services.Data/Messages/IMessageService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace YourMoviesForum.Services.Data.Messages
-{
-    public interface IMessageService
-    {
-        Task CreateAsync(string content, string authorId, string receiverId);
-    }
-}
