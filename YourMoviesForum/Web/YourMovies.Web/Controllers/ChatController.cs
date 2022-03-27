0d37feb5350b563fdfb0b736ece6e53befd9be0a
Petar <petio567@abv.bg>
2022-03-27 20:48:10 +0300

    Changed the logic for chat view

--- YourMoviesForum/Web/YourMovies.Web/Controllers/ChatController.cs
+++ YourMoviesForum/Web/YourMovies.Web/Controllers/ChatController.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using YourMovies.Web.Infrastructure;
 using YourMoviesForum.Services.Data.Messages;
 using YourMoviesForum.Services.Data.Users;
@@ -7,7 +10,8 @@ using YourMoviesForum.Web.InputModels.Chat;
 
 namespace YourMovies.Web.Controllers
 {
-    public class ChatController:Controller
+    [Authorize]
+    public class ChatController : Controller
     {
         private readonly IUserService userService;
         private readonly IMessageService messageService;
@@ -20,21 +24,43 @@ namespace YourMovies.Web.Controllers
 
         public async Task<IActionResult> Message()
         {
-            var recievedMessages = await messageService.GetAllMessagesAsync<ChatConversationViewModel>(User.Id());
+            var viewModel = new ChatMessageInputModel();
+            var recievedMessages = await RecievedMessagesAndActivityAsync(viewModel);
 
+            viewModel.Users = await userService.GetAllUsersAsync<ChatUserViewModel>();
+            viewModel.RecievedMessages = recievedMessages;
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Message(ChatMessageInputModel input)
+        {
+            if (!ModelState.IsValid)
+            {
+                input.Users = await userService.GetAllUsersAsync<ChatUserViewModel>();
+                input.RecievedMessages=await RecievedMessagesAndActivityAsync(input);
+                return View(input);
+            }
+
+            input.Users = await userService.GetAllUsersAsync<ChatUserViewModel>();
+            input.RecievedMessages=await RecievedMessagesAndActivityAsync(input);
+
+            await messageService.CreateMessageAsync(input.Message, User.Id(), input.ReceiverId);
+
+            return View(input);
+        }
+
+        private async Task<IEnumerable<ChatConversationViewModel>> RecievedMessagesAndActivityAsync(ChatMessageInputModel input)
+        {
+            var recievedMessages = input.RecievedMessages = await messageService.GetAllMessagesAsync<ChatConversationViewModel>(User.Id());
             foreach (var user in recievedMessages)
             {
                 user.LastMessage = await messageService.GetLastMessageAsync(User.Id(), user.Id);
                 user.LastMessageActivity = await messageService.GetLastActivityAsync(User.Id(), user.Id);
             }
 
-            var viewModel = new ChatMessageInputModel
-            {
-                Users = await this.userService.GetAllUsersAsync<ChatUserViewModel>(),
-                RecievedMessages= recievedMessages
-            };
-
-            return this.View(viewModel);
+            return recievedMessages;
         }
     }
 }
